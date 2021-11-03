fda6fa242e62e93ad9c4b564f4702f8be0162d70
letenothibaudl@gmail.com <letenothibaudl@gmail.com>
2021-11-03 20:41:04 +0100

    update imputation

--- imputation.py
+++ imputation.py
@@ -9,6 +9,22 @@ from icecream import ic
 
 
 def coordinate_based_imputation_train(X, n_neighbors=3, remove_precip=True):
+    list_index_day = list(set(X['index_day'].tolist()))
+    list_index_hour = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17',
+                       '18', '19', '20', '21', '22', '23']
+
+    data = pd.DataFrame(columns=X.columns)
+    for day in list_index_day:
+        for hour in list_index_hour:
+            print(day, hour)
+            subset_data = X.loc[(X['hour'] == hour) & (X['index_day'] == day)]
+            data_to_append = subset_coordinate_based_imputation_train(subset_data)
+            # print(data_to_append)
+            data = data.append(data_to_append)
+    return data
+
+
+def subset_coordinate_based_imputation_train(X, n_neighbors=3, remove_precip=True):
     if remove_precip:
         # Search for the NaN in the 'precip' columns and remove the corresponding rows (7%)
         X = X[X['precip'].notna()]
@@ -37,17 +53,25 @@ def coordinate_based_imputation_train(X, n_neighbors=3, remove_precip=True):
 
     for feature in X.columns.tolist():
         start = timeit.default_timer()
-        knn_dataframe[feature] = X[[feature]]
-        ic(knn_dataframe)
-        print('# of missing values in col:', feature, knn_dataframe.isnull().sum().sum())
+        print(X.loc[:, [feature]])
+        knn_dataframe[feature] = X.loc[:, [feature]]  # X[[feature]]
+        # ic(knn_dataframe)
+        # print('# of missing values in col:', feature, knn_dataframe.isnull().sum().sum())
         del X[feature]
         imputer = KNNImputer(missing_values=np.nan, n_neighbors=n_neighbors,
                              metric='nan_euclidean', weights='distance')
         temp_data = imputer.fit_transform(knn_dataframe)
-        print(temp_data)
-        new_dataframe[feature] = temp_data[0:, 4]
+        # print
+        try:
+            print(feature)
+            print(temp_data)
+            new_dataframe[feature] = temp_data[0:, 4]
+        except:
+            print(feature)
+            print(temp_data)
+            exit(0)
         stop = timeit.default_timer()
-        print('Running Time ' + feature + ':', stop - start)
+        # print('Running Time ' + feature + ':', stop - start)
         del knn_dataframe[feature]
     new_dataframe['lon'] = knn_dataframe[['lon']]
     new_dataframe['lat'] = knn_dataframe[['lat']]
@@ -130,7 +154,7 @@ def knn_imputation(X, type='knn_coord', n_neighbors=3, fill_value= 0, remove_pre
     :param remove_precip: boolean to remove the rows where there is a NaN in the precip column
     :return:
     """
-    if type == "coord":
+    if type == "knn_coord":
         return coordinate_based_imputation(X, n_neighbors, )
     elif type == 'knn':
         if remove_precip:
@@ -160,6 +184,7 @@ def fill_Y_train_Nan(path_station_coordinates, path_Y_data, n_neighbors=3):
 if __name__ == '__main__':
     ic.configureOutput(includeContext=True)
     path_station_coordinates = '.././/Other/stations_coordinates.csv'
+
     """path_X_data = '.././Train/Train/X_station_train.csv'
     path_Y_data = '.././Train/Train/Y_train.csv'
     # y = fill_Y_train_Nan(path_station_coordinates, path_Y_data)
@@ -171,9 +196,27 @@ if __name__ == '__main__':
     ic(new_X.columns.tolist())
     ic(new_X.isnull().sum().sum())"""
 
-    path_X_data = '.././Test/Test/X_station_test.csv'
-    X = get_clean_data(path_station_coordinates, path_X_data, 'test')
-    X = X.head(1000)
+    path_X_data = '.././Train/Train/X_station_train.csv'
+    X = get_clean_data(path_station_coordinates, path_X_data, 'train')
+    ic(X.columns.tolist())
+    display(X.shape)
     ic(X.isnull().sum().sum())
-    new_X = coordinate_based_imputation(X, 'test', remove_precip=False)
+    new_X = coordinate_based_imputation(X, 'train', remove_precip=False)
+    print(new_X)
+    display(new_X.shape)
     ic(new_X.isnull().sum().sum())
+
+    # ic(new_X.isnull().sum().sum())
+
+    """
+    Traceback (most recent call last):
+  File "/home/thibaud/Documents/M2_MLDM/ML_project/Defi-IA-2022-Rain-Frogs/imputation.py", line 195, in <module>
+    new_X = coordinate_based_imputation(X, 'train', remove_precip=False)
+  File "/home/thibaud/Documents/M2_MLDM/ML_project/Defi-IA-2022-Rain-Frogs/imputation.py", line 129, in coordinate_based_imputation
+    return coordinate_based_imputation_train(X, n_neighbors=n_neighbors, remove_precip=remove_precip)
+  File "/home/thibaud/Documents/M2_MLDM/ML_project/Defi-IA-2022-Rain-Frogs/imputation.py", line 20, in coordinate_based_imputation_train
+    data_to_append = subset_coordinate_based_imputation_train(subset_data)
+  File "/home/thibaud/Documents/M2_MLDM/ML_project/Defi-IA-2022-Rain-Frogs/imputation.py", line 63, in subset_coordinate_based_imputation_train
+    new_dataframe[feature] = temp_data[0:, 4]
+IndexError: index 4 is out of bounds for axis 1 with size 4 
+    """
